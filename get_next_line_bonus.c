501e41daa938fc87a0f4dafa2d9dd51956b86d3a
Yann Erbs <yerbs@k0r3p2.42mulhouse.fr>
2023-11-13 09:39:04 +0100

    gnl

---
+++ get_next_line_bonus.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerbs <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 08:52:03 by yerbs             #+#    #+#             */
+/*   Updated: 2023/11/13 08:52:04 by yerbs            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_check(int br, char **line)
+{
+	if (br == -1 || !(*line) || (*line)[0] == '\0')
+	{
+		free(*line);
+		return (0);
+	}
+	return (1);
+}
+
+void	ft_update(char **line, char *buffer)
+{
+	int		i;
+	int		len;
+	char	*tmp;
+
+	i = 0;
+	while ((*line)[i] && (*line)[i] != '\n')
+		i++;
+	len = ft_strlen(*line + i + 1);
+	ft_memmove(buffer, *line + i + 1, len);
+	buffer[len] = '\0';
+	if ((*line)[i] == '\n')
+	{
+		tmp = ft_substr(*line, 0, i + 1);
+		if (!tmp)
+			return ;
+		free(*line);
+		*line = tmp;
+	}
+}
+
+void	ft_read(int fd, char **line, char *buffer, int *br)
+{
+	*br = 1;
+	while (!ft_strchr(buffer, '\n') && *br != 0)
+	{
+		*br = read(fd, buffer, BUFFER_SIZE);
+		if (*br == -1)
+		{
+			buffer[0] = '\0';
+			free(*line);
+			*line = NULL;
+			return ;
+		}
+		buffer[*br] = '\0';
+		*line = ft_strjoin(*line, buffer);
+		if (!*line)
+			return ;
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[1024][BUFFER_SIZE + 1];
+	int			br;
+	char		*line;
+
+	if (fd < 0 || fd >= 1024 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = NULL;
+	if (buffer[fd][0] != '\0')
+		line = ft_strjoin(NULL, buffer[fd]);
+	ft_read(fd, &line, buffer[fd], &br);
+	if (!ft_check(br, &line))
+		return (NULL);
+	if (br == 0)
+		return (line);
+	ft_update(&line, buffer[fd]);
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerbs <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 09:00:36 by yerbs             #+#    #+#             */
+/*   Updated: 2023/11/13 09:00:40 by yerbs            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*ft_strchr(const char *str, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *line, char *buff);
+
+size_t	ft_strlen(const char *str);
+
+void	ft_update(char **line, char *buffer);
+void	ft_read(int fd, char **line, char *buffer, int *br);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+int		ft_check(int br, char **line);
+
+#endif
